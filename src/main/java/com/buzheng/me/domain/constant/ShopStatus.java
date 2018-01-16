4b14158496f0d6d9c0d13bef9e787490ec7f55a7
buzheng <yupeibiaodeveloper@yeah.net>
2018-01-16 09:53:01 +0800

    增加店铺状态枚举类

---
+++ src/main/java/com/buzheng/me/domain/constant/ShopStatus.java
@@ -0,0 +1,53 @@
+package com.buzheng.me.domain.constant;
+
+import com.buzheng.me.domain.entity.Shop;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by buzheng on 18/1/16.
+ * 店铺状态枚举类
+ */
+public enum ShopStatus {
+    CHECK(1001, "店铺审核中"),
+    ILLEGAL(1002, "非法店铺"),
+    SUCCESS(1003, "操作成功"),
+    PASS(1004, "认证通过"),
+    ERROR(1005, "内部异常");
+    @Getter
+    @Setter
+    /**
+     * 店铺状态值
+     */
+    private Integer status;
+
+    @Getter
+    @Setter
+    /**
+     * 信息
+     */
+    private String message;
+
+    private ShopStatus(Integer status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    /**
+     * 根据status获取枚举值
+     *
+     * @param status
+     * @return
+     * @see ShopStatus
+     */
+    public static ShopStatus statusOf(int status) {
+        ShopStatus matchStatus = ShopStatus.ERROR;
+        for (ShopStatus shopStatus : values()) {
+            if (shopStatus.status == status) {
+                matchStatus = shopStatus;
+                break;
+            }
+        }
+        return matchStatus;
+    }
+}
